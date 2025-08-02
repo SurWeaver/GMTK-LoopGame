b4f5a2e55ff50b6f557a86e5022e46e8bf824e40
Alexander Tkachenko <xander.tka4enko@gmail.com>
2025-08-02 22:12:42 +0700

    Added random functions

--- autoload/random.gd
+++ autoload/random.gd
@@ -10,3 +10,20 @@ func _ready() -> void:
 
 func get_float(from: float, to: float) -> float:
 	return rng.randf_range(from, to)
+
+
+func get_random_child(node: Node) -> Node:
+	var child_count = node.get_child_count()
+	return node.get_child(get_random_index(child_count))
+
+func get_unrepeated_indices(size: int, count: int) -> Array[int]:
+	var indices: Array[int] = []
+
+	while indices.size() < count:
+		var random_index = get_random_index(size)
+		if random_index not in indices:
+			indices.append(random_index)
+	return indices
+
+func get_random_index(size: int) -> int:
+	return rng.randi_range(0, size - 1)
