8e5df728b04a8df379b8dd4646b23b4a2f067811
Alexander Tkachenko <xander.tka4enko@gmail.com>
2025-08-02 22:40:35 +0700

    Added horde function

--- models/level/level_horde.gd
+++ models/level/level_horde.gd
@@ -4,3 +4,6 @@ extends Resource
 
 @export var targets: Dictionary[int, PackedScene]
 @export var waves: Array[LevelWave]
+
+func get_target_new_scene(target_index: int) -> Target:
+	return targets[target_index].instantiate()
