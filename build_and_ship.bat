723c9cb7207007861dac38780ebb8ad75dd9f9f3
Alexander Tkachenko <xander.tka4enko@gmail.com>
2025-08-03 17:09:34 +0700

    Changed shipping settings

--- build_and_ship.bat
+++ build_and_ship.bat
@@ -1,7 +1,5 @@
 godot --headless --export-release "Windows Desktop"
 godot --headless --export-release "Linux"
-godot --headless --export-release "macOS"
 
 butler push ./builds/win SurWeaver/3r:win --userversion-file ./builds/version.txt --if-changed --ignore '*.txt'
-butler push ./builds/win SurWeaver/3r:linux --userversion-file ./builds/version.txt --if-changed --ignore '*.txt'
-butler push ./builds/win SurWeaver/3r:mac --userversion-file ./builds/version.txt --if-changed --ignore '*.txt'
+butler push ./builds/linux SurWeaver/3r:linux --userversion-file ./builds/version.txt --if-changed --ignore '*.txt'
