8f7202f9746be3e407d8340e89498f6953fac59c
Alexander Tkachenko <xander.tka4enko@gmail.com>
2025-08-03 11:28:24 +0700

    Updated resources

--- autoload/resources.gd
+++ autoload/resources.gd
@@ -3,11 +3,14 @@ extends Node
 
 const TEMPLATES = {
 	"default_barrel": preload("res://data/barrels/default_barrel.tres"),
+	"damage_number": preload("res://assets/ui/damage_number.tscn"),
 }
 
 const LEVELS: LevelCollection = preload("res://data/levels/levels.tres")
 
 const PATHS = {
 	"user_save": "user://save.res",
-	"user_barrels": "user://barrels.res"
+	"user_barrels": "user://barrels.res",
+	"level": "res://scenes/templates/level.tscn",
+	"hub": "res://scenes/level_hub.tscn",
 }
